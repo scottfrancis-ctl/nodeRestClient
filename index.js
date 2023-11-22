4c713a9e48aa63525dc76d6224afe865ad65df66
ScottFrancis <scott.francis@lumen.com>
2023-11-22 15:09:50 -0700

    manual json add for test

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ const app = express();
 
 const port = 3000;
 
-const typicode = require('./indexTypicodeHostRef');
+//const typicode = require('./indexTypicodeHostRef');
+
+app.use(express.json());  // middleware
 
 app.get('/', (req, res) => {
+    res.contentType('application/json');
     res.send("hello world");
-    res.send(`${typicode.byIdResult}`);
+
+    var people = [
+        { name: 'Dave', location: 'Atlanta' },
+        { name: 'Santa Claus', location: 'North Pole' },
+        { name: 'Man in the Moon', location: 'The Moon' }
+      ];
+
+    var PeopleJson = JSON.stringify(people);
+    
+    //res.send(`${typicode.byIdResult}`);
     //res.send(typicode.byIdResult);
+    res.send(PeopleJson);
 });
 
 app.listen(port, () => {
